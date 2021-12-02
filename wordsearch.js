5719d9dd9f3feb235cc973c37b9dea7634ecd8d8
mhakbar <habibakbar@live.com>
2021-12-02 13:12:43 -0800

    completed the word search activity

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,14 @@
-const wordSearch = (letters, word) => { 
+const wordSearch = (letters, word) => {
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        if (horizontalJoin.includes(word)){
+          return true
+        } else {
+          return false;
+        }
     }
 }
 
 module.exports = wordSearch
+
+//Pair programming by Md Hefzul Bari & Muhammad Akbar
