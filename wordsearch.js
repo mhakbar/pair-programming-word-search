38a16033938fd228868832dbc8a95910335d823d
mhakbar <habibakbar@live.com>
2022-01-19 19:30:38 -0800

    groupassingment completed with Md Meer & Muhammad Akbar

--- wordsearch.js
+++ wordsearch.js
@@ -1,14 +1,20 @@
-const wordSearch = (letters, word) => {
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (horizontalJoin.includes(word)){
-          return true
-        } else {
-          return false;
-        }
-    }
+const wordSearch = (letters, word) => { 
+  let val = true;
+  const horizontalJoin = letters.map(ls => ls.join(''))
+  for (l of horizontalJoin) {
+      if (horizontalJoin.includes(word)) {
+          val = true;
+      } else {
+          val = false;
+      }
+      
 }
+return val;
+};
 
-module.exports = wordSearch
 
-//Pair programming by Md Hefzul Bari & Muhammad Akbar
+module.exports = wordSearch;
+
+/*Pair Programming assignment = pair-programming-word-search
+By: Md Meer and Muhammad Akbar 
+time to complete = 45 mins */
